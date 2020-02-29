c541aea07b26cb3a1d89a445266291cbe1b82287
seymanurkucuk <seymanur2518@gmail.com>
2020-02-29 11:33:35 -0800

    update LogOut.java

--- src/test/LogOut.java
+++ src/test/LogOut.java
@@ -5,5 +5,11 @@ import org.w3c.dom.ls.LSOutput;
 public class LogOut {
     public static void main(String[] args) {
         System.out.println("Seymanur");
+        //updated new code
+        System.out.println("Omer Faruk");
+
+
+
+
     }
 }
